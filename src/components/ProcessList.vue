b689f6171f5c6649daa9f1c344804b0dc60bf25d
Todd Sampson <tsampson@cloudspace.com>
2020-04-24 18:18:33 -0400

    Added descriptions to model steps

--- src/components/ProcessList.vue
+++ src/components/ProcessList.vue
@@ -2,10 +2,13 @@
   <v-card>
     <v-img :src="`https://cdn.sanity.io/${processStep.stepImage.asset.path}?w=600`" />
     <v-card-title>{{ processStep.name }}</v-card-title>
+    <block-content :blocks="processStep._rawDescription" />
+    <!-- <v-card-text>{{ processStep._rawDescription }}</v-card-text> -->
   </v-card>
 </template>
 
 <script>
+import BlockContent from 'sanity-blocks-vue-component'
 export default {
   props: ['processStep']
 }
